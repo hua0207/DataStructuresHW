e0932fb42d78d127cf845581597bd44fb49eb913
hua0207 <hua0207@outlook.com>
2015-10-04 18:12:34 +0800

    data structures chapter 2: create matrix

---
+++ Chapter2_Array/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ShowDataA = New System.Windows.Forms.TextBox()
+        Me.CreateMatrixA = New System.Windows.Forms.Button()
+        Me.ShowDataB = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ShowDataA
+        '
+        Me.ShowDataA.Location = New System.Drawing.Point(12, 66)
+        Me.ShowDataA.Multiline = True
+        Me.ShowDataA.Name = "ShowDataA"
+        Me.ShowDataA.Size = New System.Drawing.Size(135, 155)
+        Me.ShowDataA.TabIndex = 0
+        '
+        'CreateMatrixA
+        '
+        Me.CreateMatrixA.Location = New System.Drawing.Point(12, 12)
+        Me.CreateMatrixA.Name = "CreateMatrixA"
+        Me.CreateMatrixA.Size = New System.Drawing.Size(115, 21)
+        Me.CreateMatrixA.TabIndex = 1
+        Me.CreateMatrixA.Text = "Create Matrix "
+        Me.CreateMatrixA.UseVisualStyleBackColor = True
+        '
+        'ShowDataB
+        '
+        Me.ShowDataB.Location = New System.Drawing.Point(185, 66)
+        Me.ShowDataB.Multiline = True
+        Me.ShowDataB.Name = "ShowDataB"
+        Me.ShowDataB.Size = New System.Drawing.Size(135, 155)
+        Me.ShowDataB.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 362)
+        Me.Controls.Add(Me.ShowDataB)
+        Me.Controls.Add(Me.CreateMatrixA)
+        Me.Controls.Add(Me.ShowDataA)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ShowDataA As System.Windows.Forms.TextBox
+    Friend WithEvents CreateMatrixA As System.Windows.Forms.Button
+    Friend WithEvents ShowDataB As System.Windows.Forms.TextBox
+
+End Class
---
+++ Chapter2_Array/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+
+    Private Function CreateMatrix(x As Integer, y As Integer) As Integer(,)
+        Dim matrixA(x, y) As Integer
+        Dim rndNum As New Random(Guid.NewGuid().GetHashCode())
+        Dim arrayNo As Integer
+
+        For i As Integer = 0 To x
+            For j As Integer = 0 To y
+                matrixA(i, j) = rndNum.Next(10)
+                'matrixA(i, j) = arrayNo
+
+                arrayNo += 1
+            Next
+        Next
+
+        Return matrixA
+    End Function
+
+    Private Function ShowMatrix(matrix(,) As Integer) As String
+
+        Dim str As String = ""
+
+        For i As Integer = 0 To UBound(matrix, 1)
+            For j As Integer = 0 To UBound(matrix, 2)
+
+                If j = UBound(matrix, 2) Then
+                    str = str + matrix(i, j).ToString
+                Else
+                    str = str + matrix(i, j).ToString + ", "
+                End If
+            Next
+
+            str += vbCrLf
+
+        Next
+
+        Return str
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub CreateMatrix_Click(sender As Object, e As EventArgs) Handles CreateMatrixA.Click
+        ShowDataA.Text = ShowMatrix(CreateMatrix(2, 2))
+        ShowDataB.Text = ShowMatrix(CreateMatrix(2, 2))
+
+    End Sub
+End Class
